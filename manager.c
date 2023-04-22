66f8a544bf5369cc673ad3bb2351d2ccd08ca30a
thisain <tjdnftjdgus@naver.com>
2023-04-22 21:46:17 +0900

    create loaddata

--- manager.c
+++ manager.c
@@ -57,10 +57,20 @@ int loadData(Product *p){
 	FILE*fp;
 
 	//파일 내용을 읽어와서 배열에 값 추가하기
-
+	fp= fopen("product.txt","rt");
+	if(fp==NULL){
+        printf("=> 파일 없음!\n");
+        return 0;
+    	}
+    while(!feof(fp)){
+        fscanf(fp,"%d",&p[count].weight);
+        fscanf(fp,"%d",&p[count].price);
+        fscanf(fp,"%[^\n]",p[count].name);
+        count++;
+    }
 
 
 
 	printf("=> 로딩 성공!\n");
-	return count;
+	return count-1;
 }
